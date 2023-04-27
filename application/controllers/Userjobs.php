73debc48271d9434aa306212da7f79a33c75af4e
Eric Mensah <ericmensah@codewrite.org>
2023-04-27 02:01:46 +0000

    initial commit

---
+++ application/controllers/Userjobs.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') or exit('No direct script allowed');
+
+class Userjobs extends MY_Controller
+{
+
+    /**
+     * Store a resource
+     * print json Response
+     */
+    public function store()
+    {
+        $gate = auth()->can('create', 'userjob');
+        if ($gate->allowed()) {
+            $record = inputJson();
+          
+            $userjob  = $this->userjob->create($record);
+            $error = $this->session->flashdata('error_message');
+           
+            if ($userjob) {
+                $out = [
+                    'data' => $userjob,
+                    'input' => $record,
+                    'status' => true,
+                    'message' => 'userjobs created successfully!'
+                ];
+            } else {
+                $out = [
+                    'status' => false,
+                    'message' => ($error?$error:"userjobs couldn't be created!")
+                ];
+            }
+        } else {
+            $out = [
+                'status' => false,
+                'message' => $gate->message
+            ];
+        }
+        httpResponseJson($out);
+    }
+
+    /**
+     * Delete a resource
+     * print json Response
+     */
+    public function delete(int $id = null)
+    {
+        $gate = auth()->can('delete', 'userjob', $this->userjob->find($id));
+        if ($gate->allowed()) {
+            if ($this->userjob->delete($id)) {
+                $out = [
+                    'status' => true,
+                    'message' => 'userjob data deleted successfully!'
+                ];
+            } else {
+                $out = [
+                    'status' => false,
+                    'message' => "userjob data couldn't be deleted!"
+                ];
+            }
+        } else {
+            $out = [
+                'status' => false,
+                'message' => $gate->message
+            ];
+        }
+        httpResponseJson($out);
+    }
+}
---
+++ application/models/Userjob_model.php
@@ -0,0 +1,159 @@
+<?php
+defined('BASEPATH') or exit('Direct acess is not allowed');
+
+class Userjob_model extends CI_Model
+{
+    public $table = 'user_jobs';
+
+    public function create(array $record)
+    {
+        if (!$record) return;
+        $record['user_id'] = auth()->user()->id;
+
+        $data = $this->extract($record);
+
+        if(isset($record['jobs'])){
+            $data2 = [];
+            foreach (explode(',',$record['jobs']) as $job_id) {
+                array_push($data2,array_merge($data, ['job_id' => $job_id]));
+            }
+            
+            $this->db->insert_batch($this->table, $data2);
+            return $this->find($record['user_id']);
+        }
+        if ($this->db->insert($this->table, $data)) {
+            return $this->find($record['user_id']);
+        }
+    }
+
+    /**
+     * Update a record
+     * @param $id
+     * @return Boolean
+     */
+    public function update(int $id, array $data)
+    {
+        $data = $this->extract($data);
+        $this->db->set($data);
+        $this->db->where('id', $id);
+        $this->db->update($this->table);
+        return $this->find($id);
+    }
+
+
+    /**
+     * Delete a record
+     * @param $id
+     * @return Boolean
+     */
+    public function delete(int $id)
+    {
+        $role = $this->find($id);
+        if ($this->perm->delete($role->permission_id))
+            return $this->db->delete($this->table, ['id' => $id]);
+        
+        return false;
+    }
+
+    /**
+     * Extract only values of only fields in the table
+     * @param $data
+     * @return Array
+     */
+    protected function extract(array $data)
+    {
+
+        // filter array for only specified table data
+        $filtered = array_filter($data, function ($key, $val) {
+            return $this->db->field_exists($val, $this->table);
+        }, ARRAY_FILTER_USE_BOTH);
+
+        return $filtered;
+    }
+
+    /**
+     * Get role by id
+     */
+    public function find(int $id)
+    {
+        if (!$id) return;
+
+        $where = [
+            'user_id' => $id,
+        ];
+        $where = [];
+        $fields = [];
+
+        foreach ($this->db->field_data($this->job->table) as $field_data) {
+            if (in_array($field_data->name, $this->job->hidden)) continue; // skip hidden fields
+            array_push($fields, "{$this->job->table}.$field_data->name");
+        }
+
+        $user_job = $this->all()
+            ->select($fields)
+            ->join('jobs', 'jobs.id=user_jobs.job_id')
+            ->where($where)
+            ->get()
+            ->result();
+        if(!$user_job) return false;
+        return $user_job;
+    }
+
+    /**
+     * Get user_jobs by column where cluase
+     */
+    public function where(array $where)
+    {
+        return $this->db->get_where($this->table, $where);
+    }
+
+    /**
+     * Get all user_jobs
+     */
+    public function all()
+    {
+        $fields = [$this->table.'.*',];
+        foreach($this->db->field_data($this->user->table) as $field_data){
+            if(in_array($field_data->name,$this->user->hidden)) continue; // skip hidden fields
+            array_push($fields, "{$this->user->table}.$field_data->name");
+        }
+
+        return
+            $this->db->select($fields, true)
+            ->join('users', "users.id={$this->table}.user_id1")
+            ->from($this->table);
+    }
+
+    public function canViewAny($user){
+        if (!auth()->authorized()) {
+            httpReponseError('Unauthorized Access!', 401);
+        }
+
+        return auth()->allow();
+    }
+
+    public function canView($user, $model){
+        if (!auth()->authorized()) {
+            httpReponseError('Unauthorized Access!', 401);
+        }
+        return auth()->allow();
+    }
+
+    public function canCreate($user){
+         if (!auth()->authorized()) {
+            httpReponseError('Unauthorized Access!', 401);
+        }
+        return auth()->allow();
+    }
+
+    public function canUpdate($user, $model){ 
+        return auth()->allow();
+    }
+
+    public function canDelete($user, $model){
+        if (!auth()->authorized()) {
+            httpReponseError('Unauthorized Access!', 401);
+        }
+        return auth()->allow();
+    }
+}
